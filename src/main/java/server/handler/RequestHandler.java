f05d7b220355a7e76f79685f60e0a93e2f48f246
Geek-Kevin2333 <q1835894540@163.com>
2022-05-23 20:22:40 +0800

    注释改变

--- src/main/java/server/handler/RequestHandler.java
+++ src/main/java/server/handler/RequestHandler.java
@@ -6,6 +6,7 @@ import message.response.HttpResponse;
 import java.net.Socket;
 
 // czh: 我是认为这部分的职责应该交给Server，但是要多线程，所以就先这么试一下，有更好的方法吗？
+// ckw: client包下的Responsehandler处理的是对从服务器端收到的相应报文做处理，RequestHandler处理的是对即将传递给服务器端的请求报文做处理
 
 public class RequestHandler extends Thread implements Handler {
     Socket socket;
