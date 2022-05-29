f2f825d0d35dc673950b38ee649e8d0a8d85dcec
ZH Cai <201250127@smail.nju.edu.cn>
2022-05-29 19:59:35 +0800

    refactor server code

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -16,7 +16,7 @@ import java.io.InputStreamReader;
  */
 public class ClientMain {
     public static void main(String[] args) throws IOException {
-        int port=808;
+        int port=8080;
         String host="127.0.0.1";
 //        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 //        System.out.println("请输入服务器host");
